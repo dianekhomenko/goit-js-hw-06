6e37564b4406b698598b785a916965cf80192380
htmldiana <dianekhomenko@gmail.com>
2023-01-27 22:17:58 +0100

    task-8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,26 @@
+const form = document.querySelector(
+  ".login-form"
+);
 
+form.addEventListener("submit", handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+
+  if (
+    email.value === "" ||
+    password.value === ""
+  ) {
+    alert("Please fill in all the fields!");
+  } else {
+    const user = {
+      email: email.value,
+      password: password.value,
+    };
+    console.log(user);
+    event.currentTarget.reset();
+  }
+}
