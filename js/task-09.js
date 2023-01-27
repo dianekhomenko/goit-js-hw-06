273d74c4413c5641169bf7baea396c50936c44ea
htmldiana <dianekhomenko@gmail.com>
2023-01-27 22:28:58 +0100

    task-09

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,19 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(
+    Math.random() * 16777215
+  ).toString(16)}`;
 }
+
+const body = document.querySelector("body");
+
+const button = document.querySelector(
+  ".change-color"
+);
+
+const span = document.querySelector(".color");
+
+button.addEventListener("click", () => {
+  let newcolor = getRandomHexColor();
+  body.style.backgroundColor = newcolor;
+  span.textContent = newcolor;
+});
